51e77df36365c0a6c651c3544270d1005134138f
vignesh <>
2025-05-15 22:14:01 +0530

    deployment

---
+++ docs/assets/table-BfN3VPLO.js
@@ -0,0 +1,12 @@
+import{c as o}from"./index-CcxtFsBt.js";import{j as t,c as s,r as l}from"./pwa-components-Dgy0VUO_.js";/**
+ * @license lucide-react v0.509.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"m21 21-4.34-4.34",key:"14j7rj"}],["circle",{cx:"11",cy:"11",r:"8",key:"4ej97u"}]],x=o("search",c);/**
+ * @license lucide-react v0.509.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=[["path",{d:"M3 6h18",key:"d0wm0j"}],["path",{d:"M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6",key:"4alrt4"}],["path",{d:"M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2",key:"v07s0e"}],["line",{x1:"10",x2:"10",y1:"11",y2:"17",key:"1uufr5"}],["line",{x1:"14",x2:"14",y1:"11",y2:"17",key:"xtxkd"}]],h=o("trash-2",n),d=l.forwardRef(({className:e,...a},r)=>t.jsx("div",{ref:r,className:s("relative w-full overflow-auto md:overflow-visible",e),...a}));d.displayName="TableResponsiveContainer";function u({className:e,...a}){return t.jsx("div",{"data-slot":"table-container",className:"relative w-full overflow-x-auto",children:t.jsx("table",{"data-slot":"table",className:s("w-full caption-bottom text-sm",e),...a})})}function m({className:e,...a}){return t.jsx("thead",{"data-slot":"table-header",className:s("[&_tr]:border-b",e),...a})}function p({className:e,...a}){return t.jsx("tbody",{"data-slot":"table-body",className:s("[&_tr:last-child]:border-0",e),...a})}function f({className:e,...a}){return t.jsx("tr",{"data-slot":"table-row",className:s("hover:bg-muted/50 data-[state=selected]:bg-muted border-b transition-colors",e),...a})}function y({className:e,...a}){return t.jsx("th",{"data-slot":"table-head",className:s("text-foreground h-10 px-2 text-left align-middle font-medium whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",e),...a})}function j({className:e,...a}){return t.jsx("td",{"data-slot":"table-cell",className:s("p-2 align-middle whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",e),...a})}export{x as S,h as T,d as a,u as b,m as c,f as d,y as e,p as f,j as g};
+//# sourceMappingURL=table-BfN3VPLO.js.map
